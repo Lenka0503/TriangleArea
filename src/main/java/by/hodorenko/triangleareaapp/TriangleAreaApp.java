e9d544a916a0cb5a3bb886a3913f836a2feae0a4
Elena_Hodorenko <alena0503@gmail.com>
2025-12-17 22:23:36 +0300

    Improve result output format

--- src/main/java/by/hodorenko/triangleareaapp/TriangleAreaApp.java
+++ src/main/java/by/hodorenko/triangleareaapp/TriangleAreaApp.java
@@ -35,7 +35,7 @@ public class TriangleAreaApp extends JFrame {
 
                 double p = (a + b + c) / 2;
                 double area = Math.sqrt(p * (p - a) * (p - b) * (p - c));
-                resultField.setText(String.format("%.2f", area));
+                resultField.setText("Площадь: " + String.format("%.2f", area));
 
             } catch (NumberFormatException ex) {
                 JOptionPane.showMessageDialog(this,
